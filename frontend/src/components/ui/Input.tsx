6a42cf657ac52d4d3bcb6b734f7b254b7df1ba5e
Fotis Koutoulakis <fotis.koutoulakis@gmail.com>
2025-08-26 09:28:23 +0100

    feat(frontend/ui): Input component - for textboxes, etc

---
+++ frontend/src/components/ui/Input.tsx
@@ -0,0 +1,10 @@
+import type {FC, InputHTMLAttributes} from "react";
+
+const Input: FC<InputHTMLAttributes<HTMLInputElement>> = ({ className = '', ...props }) => {
+    return (
+        <input
+            className={`flex h-10 w-full rounded-md border border-slate-300 bg-transparent py-2 px-3 text-sm placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 ${className}`}
+            {...props}
+        />
+    );
+};
