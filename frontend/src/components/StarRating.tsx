bcdf52849c431b665551d1ba1cae587cc7b575fe
Fotis Koutoulakis <fotis.koutoulakis@gmail.com>
2025-08-26 21:17:57 +0100

    feat(frontend): add a component to help implement a star system

---
+++ frontend/src/components/StarRating.tsx
@@ -0,0 +1,15 @@
+import type {FC} from "react";
+
+export const StarRating: FC<{ rating: number }> = ({ rating }) => {
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+        if (i <= rating) {
+            stars.push(<svg key={i} className="w-4 h-4 text-amber-400 fill-current" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/></svg>);
+        } else if (i - 0.5 <= rating) {
+            stars.push(<svg key={i} className="w-4 h-4 text-amber-400 fill-current" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z M10 12.425V0l2.061 4.18 4.62.67-3.34 3.26.79 4.6-4.13-2.17z"/></svg>);
+        } else {
+            stars.push(<svg key={i} className="w-4 h-4 text-slate-300 fill-current" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/></svg>);
+        }
+    }
+    return <div className="flex items-center">{stars}</div>;
+};
